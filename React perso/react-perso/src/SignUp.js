c1d8f4ccb06c52eb2a73e760fe8b3c3f92e37c87
anthony ciszowski <anthonyciszowski@gmail.com>
2019-03-07 13:48:11 +0100

    quete 010

--- React perso/react-perso/src/SignUp.js
+++ React perso/react-perso/src/SignUp.js
@@ -4,25 +4,35 @@ import './style.css'
 class SignUp extends React.Component{
     constructor(props){
         super(props);
-        this.tab = [];
         this.state= {
             email: '',
             password: '',
             name:'',
             lastname:'',
+            confirm:  '',
             valeur : []
         }
     }
 
     handleClick=(e)=>{
-
+        const {email, password, name, lastname , confirm} = this.state
         e.preventDefault();
-        let jsonTab = []
-        jsonTab.push(JSON.stringify(this.state, 1,1))
 
-        this.setState({
-            valeur : jsonTab
-        })
+        let stateVal = []
+        stateVal.push(email, password, name , lastname)
+
+        if( password ===  confirm) {
+            let jsonTab = []
+            jsonTab.push(JSON.stringify(stateVal, 1,1))
+    
+            this.setState({
+                valeur : jsonTab
+            })
+        }else{
+            throw new Error('Different password manip ')
+        }
+
+
     }
 
     onChange = (e) => {
@@ -46,7 +56,7 @@ class SignUp extends React.Component{
                     <input id='mdp' type="password" name="password" onChange={this.onChange} />
 
                     <label htmlFor='verif'> Confirmez mot de passe </label> 
-                    <input id='verif' type="password" name="password" onChange={this.onChange} />
+                    <input id='verif' type="password" name="confirm" onChange={this.onChange} />
 
                     <label htmlFor='prenom'> prenom</label> 
                     <input id='prenom' type="text" name="name"onChange={this.onChange} />
